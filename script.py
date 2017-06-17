22f4c32d8b61151d257bad3589278a99d1b11368
JonMoore9000 <jonmoore9000@gmail.com>
2017-06-16 23:49:40 -0500

    python push

---
+++ script.py
@@ -0,0 +1,5 @@
+thing = raw_input("what is your name?")
+age = raw_input("how old are you?")
+stuff = raw_input("what are you doing?")
+
+print "So you are %s and you are %s years old? Why are you %s?" % (thing, age, stuff)
